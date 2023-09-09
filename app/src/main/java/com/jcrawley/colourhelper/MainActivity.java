647c548c26f80d43201933d11bcb6c1953edc51c
john <johncrawley1@gmail.com>
2023-09-09 23:59:35 +0100

    Adding methods to add source image from camera.

--- app/src/main/java/com/jcrawley/colourhelper/MainActivity.java
+++ app/src/main/java/com/jcrawley/colourhelper/MainActivity.java
@@ -1,16 +1,24 @@
 package com.jcrawley.colourhelper;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +27,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView srcImageView;
     private TextView rgbTextView;
     private  int[] imageViewCoordinates;
     private View selectedColorView;
+    private ActivityResultLauncher<Intent> cameraActivityResultLauncher;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupViews();
+        initActivityResultLauncherForCamera();
     }
 
 
@@ -122,5 +138,68 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, getString(R.string.copied_to_clipboard_toast), Toast.LENGTH_SHORT).show();
     }
 
+
+    private void initActivityResultLauncherForCamera(){
+        cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result == null){
+                        return;
+                    }
+                    if (result.getResultCode() == RESULT_OK) {
+                        loadImage(currentPhotoPath);
+                    }
+                });
+    }
+
+
+    private void startTakePictureActivity(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photoFile = createTempImageFile();
+        if (photoFile == null) {
+            Toast.makeText(MainActivity.this, "unable to create temp image file", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Uri photoURI = FileProvider.getUriForFile(this, "com.jcrawley.android.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        cameraActivityResultLauncher.launch(intent);
+    }
+
+    private File createTempImageFile(){
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return photoFile;
+    }
+
+    private File createImageFile() throws IOException {
+        String imageFileName = "saved_photo_" + System.currentTimeMillis() + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(imageFileName, ".jpg", storageDir );
+        currentPhotoPath = imageFile.getAbsolutePath();
+        return imageFile;
+    }
+
+
+    public void loadImage(String path){
+        Uri uri = Uri.parse(path);
+        if(uri == null){
+            return;
+        }
+        try{
+            InputStream input = getContentResolver().openInputStream(uri);
+            if(input == null){
+                return;
+            }
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+            srcImageView.setBackground(bitmapDrawable);
+        }catch (IOException e){
+          e.printStackTrace();
+        }
+    }
 
 }
